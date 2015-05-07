104b724e84d67c213057a58e60e7539f8f754406
Anqing Ma <maanqing@smartdevices.com.cn>
2015-05-07 18:30:09 +0800

    add login layout

---
+++ app/src/main/java/com/hm/madroid/mood/ui/LoginActivity.java
@@ -0,0 +1,93 @@
+package com.hm.madroid.mood.ui;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.ContentResolver;
+import android.content.CursorLoader;
+import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.hm.madroid.mood.MainActivity;
+import com.hm.madroid.mood.R;
+
+/**
+ * A login screen that offers login via email/password and via Google+ sign in.
+ * <p/>
+ * ************ IMPORTANT SETUP NOTES: ************
+ * In order for Google+ sign in to work with your app, you must first go to:
+ * https://developers.google.com/+/mobile/android/getting-started#step_1_enable_the_google_api
+ * and follow the steps in "Step 1" to create an OAuth 2.0 client for your package.
+ */
+public class LoginActivity extends BaseActivity {
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world"
+    };
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+
+    // UI references.
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mEmailLoginFormView;
+    private SignInButton mPlusSignInButton;
+    private View mSignOutButtons;
+    private View mLoginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        initView();
+    }
+
+    private void initView(){
+
+        mLoginButton = findViewById(R.id.email_sign_in_button) ;
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.email_sign_in_button :
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
+}
+
