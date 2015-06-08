6c88d13b6565c6716fef8081441e3d6f9765231a
anqingma <madroidmaq@gmail.com>
2015-06-08 17:56:07 +0800

    update register layout

--- app/src/main/java/com/hm/madroid/mood/ui/RegisterFragment.java
+++ app/src/main/java/com/hm/madroid/mood/ui/RegisterFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.hm.madroid.mood.R;
 
@@ -25,7 +26,7 @@ public class RegisterFragment extends Fragment {
     }
 
     private void initView(View view){
-
+        ((TextView)view.findViewById(R.id.home_back)).setText(R.string.user_register);
     }
 
 
